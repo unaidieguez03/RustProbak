236bbfbe99b74d7a427cc475ad76b82756696543
unaidieguez03 <unaidieguez03@gmail.com>
2025-07-08 10:59:33 +0200

    first triangle

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,26 @@ use macroquad::prelude::*;
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
-let mut a = 1.0;
-        let (j,k) = (screen_width()/2.0, screen_height()/2.0);
-         print!("{} {}", j, k);loop {
+         loop {
         clear_background(LIGHTGRAY);
         
-        a+=0.01; 
         // Llamar a la función async y obtener el resultado
         let (new_x, new_y) = get_inputs(x, y).await;
         x = new_x;
         y = new_y;
-        draw_circle(j+a, k+a, 15.0, BLUE);
-        draw_circle(x, y, 15.0, YELLOW);
+        let vec1:Vec2 =create_vector(x-100.0, y+100.0);
+        let vec2:Vec2 =create_vector(x+100.0, y+100.0);
+        let vec3:Vec2 =create_vector(x-100.0, y-100.0);
+        let vec4:Vec2 =create_vector(x+100.0, y-100.0);
+        draw_triangle(vec1, vec2, vec3, YELLOW);
+        draw_triangle(vec3, vec2, vec4, GREEN);
         draw_text("move the ball with arrow keys", 20.0, 20.0, 20.0, DARKGRAY);
         next_frame().await;
     }
+}
+fn create_vector(x: f32, y: f32)->Vec2{
+    let return_vector = Vec2::new(x, y);
+    return_vector
 }
 
 async fn get_inputs(x: f32, y: f32) -> (f32, f32) {
