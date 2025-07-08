90fa58ff907689760a747ca772caf75529664a10
unaidieguez03 <unaidieguez03@gmail.com>
2025-07-08 12:18:50 +0200

    aer

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use macroquad::prelude::*;
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+    let mut angle = 0.0;
          loop {
         clear_background(LIGHTGRAY);
         
         // Llamar a la función async y obtener el resultado
-        let (new_x, new_y) = get_inputs(x, y).await;
-        x = new_x;
-        y = new_y;
+        let new_angle = get_angle(angle).await;
+
+        if(angle!=new_angle){
+            x = x * f32::cos(angle) - y *f32::sin(angle); 
+            y = x * f32::sin(angle) + y *f32::cos(angle); 
+        }
+        angle = new_angle;
+        print!("angle:{} , x:{}, y:{}\n", angle, x, y);
         let vec1:Vec2 =create_vector(x-100.0, y+100.0);
         let vec2:Vec2 =create_vector(x+100.0, y+100.0);
         let vec3:Vec2 =create_vector(x-100.0, y-100.0);
@@ -26,6 +32,22 @@ fn create_vector(x: f32, y: f32)->Vec2{
     return_vector
 }
 
+async fn get_angle(angle:f32) -> f32 {
+    let mut angle:f32= angle;
+    if is_key_down(KeyCode::Right) {
+        angle = 0.0;
+    }
+    if is_key_down(KeyCode::Left) {
+        angle = 180.0;
+    }
+    if is_key_down(KeyCode::Down) {
+        angle = 270.0;
+    }
+    if is_key_down(KeyCode::Up) {
+        angle = 90.0;
+    }
+    angle
+}
 async fn get_inputs(x: f32, y: f32) -> (f32, f32) {
     let mut x = x;
     let mut y = y;
